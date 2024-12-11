a0f0964ea7475b8b5701614f62b22b9ccbe62d4a
Nikvitya <nikvitya@yandex.ru>
2024-12-11 18:04:38 +0300

    feature: добавил swagger

--- user-service/src/main/java/ru/yandex/exception/handler/ErrorResponse.java
+++ user-service/src/main/java/ru/yandex/exception/handler/ErrorResponse.java
@@ -1,11 +1,14 @@
 package ru.yandex.exception.handler;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
 public class ErrorResponse {
+    @Schema(description = "Общая причина ошибки")
     private final String error;
+    @Schema(description = "Описание ошибки")
     private final String description;
 }
--- user-service/src/main/java/ru/yandex/model/dto/UserCreateDTO.java
+++ user-service/src/main/java/ru/yandex/model/dto/UserCreateDTO.java
@@ -2,6 +2,7 @@ package ru.yandex.model.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
@@ -17,16 +18,20 @@ import lombok.NoArgsConstructor;
 public class UserCreateDTO {
 
     @NotBlank(message = "Имя обязательно")
+    @Schema(description = "Имя пользователя", example = "Виталий")
     private String name;
 
     @JsonProperty("e_mail")
     @Email
     @NotBlank
+    @Schema(description = "Email пользователя", example = "vitaly@ya.ru")
     private String email;
 
     @NotBlank
+    @Schema(description = "Пароль пользователя", example = "qwerty")
     private String password;
 
     @JsonProperty("about_me")
+    @Schema(description = "Краткое описание", example = "Замечательный человек! :)")
     private String aboutMe;
 }
